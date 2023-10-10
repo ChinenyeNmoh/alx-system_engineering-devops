3a01cd2b493b88368e96aa21321a25f28fb6d3b7
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-10-10 12:52:06 +0100

    advancedapi

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
-query a subreddit without authentification
+Query a subreddit without authentication
 """
 import requests
 
 
 def top_ten(subreddit):
-	"""
-	prints the titles of the first 10 hot posts listed for a given subreddit
-	"""
-	url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-	headers = {"User-agent": "Chrome/117.0.0.0 Safari/537.36"}
-	response = requests.get(url, headers=headers)
-	if response.status_code == 200:
-		data = response.json()
-		for post in data['data']['children']:
-			T = post['data']['title']
-			print(T)
-	else:
-		print('None')
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-agent": "Chrome/117.0.0.0 Safari/537.36"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            T = post['data']['title']
+            print(T)
+    else:
+        print('None')
